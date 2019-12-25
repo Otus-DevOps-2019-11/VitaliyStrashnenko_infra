09ed54b4a5c4a7c865d724efd2a495f4ba16bcf1
VitaliyStrashnenko <smitwerty@gmail.com>
2019-12-25 13:45:38 +0000

    cloud-testapp

--- startup_script.sh
+++ startup_script.sh
@@ -2,7 +2,6 @@
 #
 sudo apt-get update
 sudo apt install -y ruby-full ruby-bundler build-essential
-
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
